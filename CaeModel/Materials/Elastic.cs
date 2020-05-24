7db21f2a6c37969d71c5018a5af70471c3062f47
Matej <matej.borovinsek@gmail.com>
2020-05-24 16:13:42 +0200

    Elastic ISerializable

--- CaeModel/Materials/Elastic.cs
+++ CaeModel/Materials/Elastic.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CaeGlobals;
+using System.Runtime.Serialization;
 
 namespace CaeModel
 {
     [Serializable]
-    public class Elastic : MaterialProperty
+    public class Elastic : MaterialProperty, ISerializable
     {
         // Variables                                                                                                                
+        [NonSerialized]
         private static string _positive = "The value must be larger than 0.";
         //
         private double _youngsModulus;
@@ -32,10 +34,38 @@ namespace CaeModel
         // Constructors                                                                                                             
         public Elastic(double youngsModulus, double poissonsRatio)
         {
+            // The constructor must wotk with E = 0
             _youngsModulus = youngsModulus;
+            // Use the method to perform any checks necessary
             PoissonsRatio = poissonsRatio;
         }
+        public Elastic(SerializationInfo info, StreamingContext context)
+        {
+            foreach (SerializationEntry entry in info)
+            {
+                switch (entry.Name)
+                {
+                    case "_youngsModulus":
+                    case "<YoungsModulus>k__BackingField":
+                        _youngsModulus = (double)entry.Value; break;
+                    case "_poissonsRatio":
+                    case "<PoissonsRatio>k__BackingField":
+                        _poissonsRatio = (double)entry.Value; break;
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+        }
+
 
         // Methods                                                                                                                  
+
+        // ISerialization
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            // using typeof() works also for null fields
+            info.AddValue("_youngsModulus", _youngsModulus, typeof(double));
+            info.AddValue("_poissonsRatio", _poissonsRatio, typeof(double));
+        }
     }
 }
