f90cbab46ac5273938981773a45270a045accaa6
Matej <matej.borovinsek@gmail.com>
2021-12-01 08:09:06 +0100

    Compatibility v1.1.1

---
+++ PrePoMax/Command/0050_Model/CSetModelUnitSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrePoMax;
+using CaeModel;
+using CaeMesh;
+using CaeGlobals;
+
+
+namespace PrePoMax.Commands
+{
+    // Compatibility v1.1.1 - the command was renamed to CSetNewModelProperties
+    [Serializable]
+    class CSetModelUnitSystem : Command
+    {
+        // Variables                                                                                                                
+        private UnitSystemType _unitSystemType;
+
+
+        // Constructor                                                                                                              
+        public CSetModelUnitSystem(UnitSystemType unitSystemType)
+            : base("Set unit system")
+        {
+            _unitSystemType = unitSystemType;
+        }
+
+
+        // Methods                                                                                                                  
+        public override bool Execute(Controller receiver)
+        {
+            receiver.SetNewModelProperties(ModelSpaceEnum.Three_D, _unitSystemType);
+            return true;
+        }
+
+        public override string GetCommandString()
+        {
+            return base.GetCommandString() + _unitSystemType.GetDescription();
+        }
+    }
+}
